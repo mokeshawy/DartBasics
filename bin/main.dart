8f21453354df917a60807be84246919eb6f926ac
Mohamed Keshawy <mokeshawy@gmail.com>
2023-05-10 12:24:55 +0300

    Data Type and Arithmetic operator

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,73 @@
-void main(){
-  print("Hello World");
+void main() {
+  // Data Type
+  /*
+  String name = 'Mohamed'; // Can you write string with single quite
+  String lastName = "Keshawy"; //Can you write string with double quite
+
+  print(name +" "+ lastName);
+
+
+  int age = 30;
+  print("Your age $age");
+  */
+
+  //Arithmetic Operators
+
+
+  //+ (addition)
+  /* int a = 4;
+  int b = 6;
+
+  int c = a + b;
+
+  print(c); */
+
+  // - (subtraction)
+
+  /* int a = 10;
+  int b = 4;
+
+  int c = a - b;
+
+  print(c); */
+
+
+ // * (multiplication)
+
+
+  /*int a = 10;
+  int b = 4;
+
+  int c = a * b;
+
+  print(c); */
+
+
+  // (division) /
+
+    // division with decimal point
+  
+    /*int a = 10;
+    int b = 2;
+    double c = a / b;
+    print(c); */
+
+
+  // division with out decimal point
+  
+    /* int a = 10;
+    int b = 2;
+    int c = a ~/ b;
+    print(c); */
+  
+  // increment operator
+  /* int a = 10;
+  a++;
+  print(a); */
+
+  // Decrement operator
+  /*int a = 10;
+  a--;
+  print(a); */
+
 }
