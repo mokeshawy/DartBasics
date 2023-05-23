1abf76d192c06b3c53612177efcee5a289ae0f9d
Mohamed Keshawy <mokeshawy@gmail.com>
2023-05-23 13:45:42 +0300

    Data for loop and while loop and do while loop

--- bin/main.dart
+++ bin/main.dart
@@ -13,7 +13,6 @@ void main() {
 
   //Arithmetic Operators
 
-
   //+ (addition)
   /* int a = 4;
   int b = 6;
@@ -31,9 +30,7 @@ void main() {
 
   print(c); */
 
-
- // * (multiplication)
-
+  // * (multiplication)
 
   /*int a = 10;
   int b = 4;
@@ -42,24 +39,22 @@ void main() {
 
   print(c); */
 
-
   // (division) /
 
-    // division with decimal point
-  
-    /*int a = 10;
+  // division with decimal point
+
+  /*int a = 10;
     int b = 2;
     double c = a / b;
     print(c); */
 
-
   // division with out decimal point
-  
-    /* int a = 10;
+
+  /* int a = 10;
     int b = 2;
     int c = a ~/ b;
     print(c); */
-  
+
   // increment operator
   /* int a = 10;
   a++;
@@ -70,4 +65,68 @@ void main() {
   a--;
   print(a); */
 
+  //------------- Type test operators -----------------//
+
+  /*int a = 10;
+
+  print(a is int); */
+
+  // String a = "W";
+  //
+  // print( a is int );
+  //
+  // print(a is! int);
+
+  int a = 15;
+
+  // a %= 2;
+  // print(a);
+  //
+  // int sex = 6;
+  // int eight = 8;
+  // if(sex == eight){
+  //   print("6");
+  // }else if(sex > eight){
+  //   print("8");
+  // }else{
+  //   print("any number");
+  // }
+
+  int price = 3000;
+
+  // switch (price) {
+  //   case 1000:
+  //     {
+  //       print("1000");
+  //     }
+  //     break;
+  //   case 2000:
+  //     {
+  //       print("2000");
+  //     }
+  //     break;
+  //   default :{
+  //     print("500");
+  //   }
+  //   break;
+  // }
+
+  // int year;
+  // for(year = 1900; year <= 2000; year++){
+  //   print(year);
+  // }
+
+  // int year = 1900;
+  // while (year <= 2000) {
+  //   if (year == 1997) break;
+  //   print(year);
+  //   year++;
+  // }
+
+
+  // int year;
+  // for(year = 1900; year <= 2000; year++){
+  //   if(year == 1950) continue;
+  //   print(year);
+  // }
 }
